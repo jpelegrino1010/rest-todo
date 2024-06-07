5f5aca6fd4de7b335f2a77ae8bee27ef6fd23285
jpelegrino1010 <julio.pelegrino@viseo.com>
2024-06-07 18:35:20 -0400

    modify pom

--- src/main/java/com/julioluis/resttodo/controller/TodoController.java
+++ src/main/java/com/julioluis/resttodo/controller/TodoController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("todos")
+@RequestMapping("api/v1/todos")
 public class TodoController {
     @Autowired
     private TodoService service;
